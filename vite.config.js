988a1e4ecd6819c5fc28e1189bb4a06ac36c37b8
eduardolm1 <eduardolm300601@gmail.com>
2025-09-12 20:10:44 +0200

    añadiendo 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
+    host: '0.0.0.0',
     proxy: {
       '/local': {
         target: 'https://twondretobackend.onrender.com/',
