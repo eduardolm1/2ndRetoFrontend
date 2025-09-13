32d3398307464b01b00a062f2028ec8f9569b28a
eduardolm1 <eduardolm300601@gmail.com>
2025-09-14 01:47:36 +0200

    implementando correcciones para vercel

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
     proxy: {
-      '/local': {
-        target: 'https://twondretobackend.onrender.com/',
+      '/api': {
+        target: 'https://second-reto-backend-89kry2awb.vercel.app',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/local/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   }
