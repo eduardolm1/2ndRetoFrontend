59cf02ec56cdd82ed4d55ba665c8d0c46a8f2387
eduardolm1 <eduardolm300601@gmail.com>
2025-09-14 02:02:43 +0200

    arreglando endpoints

--- src/redux/auth/authService.js
+++ src/redux/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api'
+const API_URL = import.meta.env.VITE_API_URL || "/api";
 
 const register = async (userData) => {
  const res = await axios.post(`${API_URL}/users/register`,userData)
--- src/redux/follow/followService.js
+++ src/redux/follow/followService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/follow";
+const API_URL = import.meta.env.VITE_API_URL || "/api/follow";
 
 const followUser = async (id) => {
   const token = JSON.parse(localStorage.getItem("token"));
